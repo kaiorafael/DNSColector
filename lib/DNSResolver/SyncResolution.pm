5909f154e70a1207e75d6e36307e46102b3dc42c
Kaio Rafael <kaiorafael@gmail.com>
2019-09-11 19:10:21 -0400

    New feature for full DNS resoluton (answer, auth, additional)
    + Default method is simple dns resolution (just answer)
    + For full mode
    	my $syncRes = DNSResolver::SyncResolution->new({
    		    domain => 'example.com',
    		    mode => 'full'
    	    });
    + clean up and coding fix
    + sample test code for understanding

--- lib/DNSResolver/SyncResolution.pm
+++ lib/DNSResolver/SyncResolution.pm
@@ -1,16 +1,18 @@
 package DNSResolver::SyncResolution;
 
 use Net::DNS::Simple;
-use Data::Dumper;
+use Carp;
 
 use warnings;
 use strict;
 use v5.20;
-no warnings "experimental";
-use feature qw(say switch);
+use feature qw(say);
 
 # domain = the domain name to be resolved (required)
 # qtype = optional having A-type resoltion as default
+# mode =
+#       minimum (default)
+#       full (all sessions)
 sub new {
     my ($class, $args) = @_;
 
@@ -19,7 +21,10 @@ sub new {
     my $self = {
         domain_name => $args->{domain},
         qtype => $args->{qtype} || 'A',
-        answer => []
+        mode => $args->{mode} || 'minimum',
+        answer => [], # Answer session
+        auth => [], # Authority session,
+        additional => [], # Additional session
     };
 
     return bless $self, $class;
@@ -30,8 +35,42 @@ sub get_domain_name {
     return $self->{domain_name};
 }
 
+# Returns DNS section
+# section:
+#   answer, auth, additional
+sub get_generic_section {
+    my ($self, $args) = @_;
+
+    if ( scalar @{$self->{ $args->{section} }} >= 1 ) {
+        return $self->{ $args->{section}} ;
+    } else {
+        carp uc($args->{section}) . " Session is empty for domain! " . $self->get_domain_name();
+        # empty response
+        return [];
+    }
+}
+
+# Return Answer Section
+sub get_answer_section {
+    my $self = shift;
+    return $self->get_generic_section({section => 'answer'});
+}
+
+# Return Auth Section
+sub get_auth_section {
+    my $self = shift;
+    return $self->get_generic_section({section => 'auth'});
+}
+
+# Return Additional Section
+sub get_additional_section {
+    my $self = shift;
+    return $self->get_generic_section({section => 'additional'});
+}
+
+
 # qtype = A, PTR, MX, NS....
-# session = answer, auth, additional, all (all sesssions)
+# send mininum or full DNS request
 sub domain_search {
     my ($self, $args) = @_;
 
@@ -40,8 +79,12 @@ sub domain_search {
         $self->{answer} = () ; # clear all results
         $self->{qtype} = $args->{qtype};
     }
-#    $self->{qtype} = $args->{qtype} if not $self->{qtype};
-    $self->send_dns_request();
+
+    # mininum request
+    $self->send_dns_request() if ( $self->{mode} eq "minimum" );
+
+    # full request
+    $self->send_full_dns_request() if ( $self->{mode} eq "full" );
 }
 
 # real dns resolution goes here
@@ -49,24 +92,70 @@ sub send_dns_request {
     my ($self, $args) = @_;
     my $res = Net::DNS::Simple->new($self->{domain_name}, $self->{qtype});
 
-    given ($res->get_rcode) {
-        when (/SERVFAIL/) { return "SERVFAIL" }
-        when (/NXDOMAIN/) { return "NXDOMAIN" }
+    if ( $res->get_rcode() ne "NOERROR" ) {
+        return "Error: " . $res->get_rcode();
     }
 
     if($res->get_ancount == 0){
         say "Answer Empty!";
-        say "---------------------------------------------";
         next;
     }
 
-    for my $entry ($res->get_answer_section()) { 
+    for my $entry ($res->get_answer_section()) {
         push @{$self->{answer}}, $entry;
     }
 }
 
+sub send_full_dns_request {
+    my ($self, $args) = @_;
+
+    ### Answer
+    $self->{qtype} = "A";
+    my $res = Net::DNS::Simple->new($self->{domain_name}, $self->{qtype});
+    if ( $res->get_rcode() eq "NOERROR" ) {
+        for my $entry ($res->get_answer_section()) {
+            push @{$self->{answer}}, $entry;
+        }
+    }
+
+    ### Auth
+    $self->{qtype} = "NS";
+    $res = Net::DNS::Simple->new($self->{domain_name}, $self->{qtype});
+    if ( $res->get_rcode() eq "NOERROR" ) {
+
+        # Auth
+        for my $entry ($res->get_answer_section()) {
+
+            # clean possible errors
+            $entry =~ s/\n//g;
+            $entry =~ s/\(//g;
+            $entry =~ s/ \)//g;
+            push @{$self->{auth}}, $entry;
+        }
+    }
+
+    ### Additional
+    ### DNS domains should have minimum 2 NS
+    ## size of auth
+    if ( scalar @{$self->{auth}} > 1 ) {
+        for my $entry (@{$self->{auth}}) {
+            ### ns entry is the last on array
+            my @ns_entry = split(/\s+/,$entry);
+            $self->{qtype} = "A";
+            $res = Net::DNS::Simple->new($ns_entry[-1], $self->{qtype});
+            if ( $res->get_rcode() eq "NOERROR" ) {
+                for my $entry ($res->get_answer_section()) {
+                    push @{$self->{additional}}, $entry;
+                }
+            }
+        }
+    }
+
+}
+
 1;
 
+=item
 #### Testing
 sub test1() {
     my $syncRes = DNSResolver::SyncResolution->new({
@@ -96,4 +185,34 @@ sub test2() {
     say $_ foreach (@{$syncRes->{answer}});
 }
 
-#test1();
+sub test3() {
+    my $syncRes = DNSResolver::SyncResolution->new({
+            domain => 'aws.com',
+            mode => 'full'
+            });
+    say $syncRes->domain_search(); # using MX-type default
+
+    say "Answer";
+    say $_ foreach (@{$syncRes->{answer}});
+
+    say "Auth";
+    say $_ foreach (@{$syncRes->{auth}});
+
+    say "Additional";
+    say $_ foreach (@{$syncRes->{additional}});
+}
+
+sub test4() {
+    my $syncRes = DNSResolver::SyncResolution->new({
+            domain => 'yahoo.com',
+            mode => 'full'
+            });
+    say $syncRes->domain_search(); # using MX-type default
+
+    say $_ foreach (@{$syncRes->get_answer_section()});
+    say $_ foreach (@{$syncRes->get_auth_section()});
+    say $_ foreach (@{$syncRes->get_additional_section()});
+}
+
+test4();
+=cut
